8083e40543294805f6cf1703ac805953e0b648e6
Fortune <fortunelangat54@gmail.com>
2022-01-30 20:06:22 +0300

    checkmate

--- assets/img/app.js
+++ assets/img/app.js
@@ -199,7 +199,7 @@ $(document).ready(function () {
     };
 
     // // send order to server
-    const response = await fetch("./api.php", {
+    const response = await fetch("api.php", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -265,7 +265,7 @@ $(document).ready(function () {
           // - Use the m-pesa endpoint
           // - Use the transaction/order data
           try {
-            const _result = await fetch("./polling.php", {
+            const _result = await fetch("polling.php", {
               method: "POST",
               headers: {
                 "Content-Type": "application/json",
